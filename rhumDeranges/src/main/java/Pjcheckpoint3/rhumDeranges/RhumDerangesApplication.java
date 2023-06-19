7034a5f020dd30bc2b4610f4e8db25d40740bccb
pjroggy <pierrejeanroggy@gmail.com>
2023-06-19 10:34:07 +0200

    first commit

---
+++ rhumDeranges/src/main/java/Pjcheckpoint3/rhumDeranges/RhumDerangesApplication.java
@@ -0,0 +1,13 @@
+package Pjcheckpoint3.rhumDeranges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhumDerangesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhumDerangesApplication.class, args);
+	}
+
+}
---
+++ rhumDeranges/src/test/java/Pjcheckpoint3/rhumDeranges/RhumDerangesApplicationTests.java
@@ -0,0 +1,13 @@
+package Pjcheckpoint3.rhumDeranges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhumDerangesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
